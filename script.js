015aecf71804a43f21af7ca2afe8f6df86dc29c4
unknown <ankit.singhania767@gmail.com>
2021-04-01 23:44:35 +0530

    updated the new font along with a script to handle the toast

---
+++ script.js
@@ -0,0 +1,19 @@
+const btnToast1 = document.querySelector(".btn-toast-1");
+const btnToast2 = document.querySelector(".btn-toast-2");
+const toast1 = document.querySelector(".toast-desc-1");
+const toast2 = document.querySelector(".toast-desc-2");
+
+btnToast1.addEventListener("click", () => {
+  console.log("Button Clicked");
+  toast1.classList.add("show");
+  setTimeout(() => {
+    toast1.classList.remove("show");
+  }, 3000);
+});
+btnToast2.addEventListener("click", () => {
+  console.log("Button Clicked");
+  toast2.classList.add("show");
+  setTimeout(() => {
+    toast2.classList.remove("show");
+  }, 3000);
+});
